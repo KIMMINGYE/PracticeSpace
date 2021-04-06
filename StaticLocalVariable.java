e78337eb0b5e53f4613e99b5e508dec02c6f1f6b
mingye <dusgml3778@gmail.com>
2021-04-06 22:26:35 +0900

    정적변수 지역변수 차이

---
+++ StaticLocalVariable.java
@@ -0,0 +1,33 @@
+package sex;
+
+public class StaticLocalVariable {
+
+	static int num = 0;
+
+	public static void main(String[] args) {
+
+		int count = 0;
+
+		while (count < 5) {
+
+			test();
+
+			count++;
+		}
+
+	}
+	
+	// static 변수는 고정기억영역에 할당되므로 지역변수와 달리 값이 계속 유지 반면에 지역변수는 함수가 호출될때마다 초기화됨
+
+	static void test() {
+
+		int a = 0;
+
+		System.out.printf("num=%d,a=%d\n", num, a);
+
+		num++;
+		a++;
+
+	}
+
+}
